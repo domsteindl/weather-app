d5ccb151c63601d675574e9b5219723d82da151e
Dominik Steindl <dominik.steindl@gmx.de>
2026-04-23 20:06:56 +0200

    add react query and api fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+} from '@tanstack/react-query'
+
 import {WEATHER_API_KEY} from "./config/env.ts";
+import Searchbar from "./features/weather/components/SearchBar.tsx";
+import {useState} from "react";
+const queryClient = new QueryClient()
 
 export default function App() {
-    console.log(WEATHER_API_KEY)
+    const [city, setCity] = useState("");
+    const {isPending, error, data} = useQuery({
+        queryKey: ['city', city],
+        queryFn: () => fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${WEATHER_API_KEY}`).then((res) => {
+           return res.json()
+        }), enabled: !!city
+    })
+
+    if (isPending) { return 'Loading'}
+    if(error) { return 'Error' + error.message + data}
+
   return (
+      <QueryClientProvider client={queryClient}>
+
+
       <main className="min-h-screen bg-[#0b1220] text-white">
 
         {/* HEADER */}
         <header className="p-6">
           <h1 className="text-3xl font-semibold">Weather App</h1>
           {/* SearchBar */}
+            <Searchbar onSearch={setCity} />
         </header>
 
         {/* MAIN CONTENT */}
@@ -27,5 +50,6 @@ export default function App() {
 
 
       </main>
+      </QueryClientProvider>
   );
 }
--- src/features/weather/components/SearchBar.tsx
+++ src/features/weather/components/SearchBar.tsx
@@ -1,3 +1,27 @@
-export default function Searchbar() {
+import {useState} from "react";
+import type { SubmitEventHandler } from "react";
+
+{/* https://api.openweathermap.org/geo/1.0/direct?q={city}&appid=848273ef355b2f00114d6d67927fcfee */}
+
+type Props = {
+    onSearch: (city: string) => void;
+};
+
+export default function Searchbar({onSearch} : Props) {
+    const [input, setInput] = useState("");
+
+    const handleSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        onSearch(input)
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+        <input
+        value={input}
+        onChange={(e) => {setInput(e.target.value)}} />
+
+        </form>
+    )
+
 
 }
