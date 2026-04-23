d0d8ee2d67a0ff59dd7cd16073e1ed478f9540c5
Dominik Steindl <dominik.steindl@gmx.de>
2026-04-23 19:05:45 +0200

    settle basic project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-
-
-
-function App() {
-
-
+export default function App() {
   return (
-    <div></div>
-  )
-}
+      <main className="min-h-screen bg-[#0b1220] text-white">
 
-export default App
+        {/* HEADER */}
+        <header className="p-6">
+          <h1 className="text-3xl font-semibold">Weather App</h1>
+          {/* SearchBar */}
+        </header>
+
+        {/* MAIN CONTENT */}
+        <section className="p-6">
+          {/* WeatherCard */}
+        </section>
+
+        <section className="p-6">
+          {/* Forecast */}
+        </section>
+
+        {/* FOOTER */}
+        <footer className="p-6 text-white/50 text-sm">
+          Data provided by OpenWeather
+        </footer>
+
+      </main>
+  );
+}
